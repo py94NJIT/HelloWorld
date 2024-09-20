27b68d93f7e99cac6563dda71dd2eb8d218fe350
py94NJIT <py94@njit.edu>
2024-09-19 20:07:09 -0400

    summary of project feature/implementations

--- myMail/app/src/main/java/com/example/mymail/MainActivity.kt
+++ myMail/app/src/main/java/com/example/mymail/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.mymail
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var emails: List<Email>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Lookup the RecyclerView in activity layout
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
+
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener{
+            val newEmails = EmailFetcher.getNext5Emails()
+            // Add new emails to existing list of emails
+            (emails as MutableList<Email>).addAll(newEmails)
+            // Notify the adapter there's new emails so the RecyclerView layout is updated
+            adapter.notifyDataSetChanged()
         }
     }
 }
